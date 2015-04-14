f6cd69cb53ec2f2c76fff91c893b77528a7475ea
Arthur Lockman <hello@rthr.me>
2015-04-13 23:47:46 -0400

    Changed cell renderer to use proper model.

--- src/main/java/model/Level.java
+++ src/main/java/model/Level.java
@@ -2,17 +2,28 @@ package model;
 
 public class Level
 {
-    String name;
-    String description;
-    int highScore;
-    int expectedScore;
-    String levelData;
-    boolean locked;
+    protected String name;
+    protected String description;
+    protected int levelNumber;
+    protected int highScore;
+    protected int expectedScore;
+    protected String levelData;
+    protected boolean locked;
 
     public Level(String data)
     {
 
+    }
 
+    public Level(String name, String description, int number,
+                 int highScore, int expectedScore, boolean locked)
+    {
+        this.name = name;
+        this.levelNumber = number;
+        this.highScore = highScore;
+        this.expectedScore = expectedScore;
+        this.locked = locked;
+        this.description = description;
     }
 
     public String getName()
@@ -28,5 +39,22 @@ public class Level
     public int getHighScore()
     {
         return highScore;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "<html><b>Level " + levelNumber + ":</b> " + description +
+                " <i>(High Score: " + highScore + ", Type: " + this.getType() + ")</i></html>";
+    }
+
+    public boolean isLocked()
+    {
+        return this.locked;
+    }
+
+    public String getType()
+    {
+        return "Puzzle";
     }
 }
--- src/main/java/view/LevelCellRenderer.java
+++ src/main/java/view/LevelCellRenderer.java
@@ -11,29 +11,17 @@ public class LevelCellRenderer extends JLabel implements ListCellRenderer<Level>
     @Override
     public Component getListCellRendererComponent(JList<? extends Level> list, Level value, int index, boolean isSelected, boolean cellHasFocus)
     {
-        String number = "Level " + value.getNumber();
+        String description = value.toString();
         ImageIcon icon;
         String rsrc = "";
-        switch (value.getType())
-        {
-            case 1: //Elimination
-                rsrc = (value.isLocked()) ? "/view/eliminationlock.png" : "/view/eliminationicn.png";
-                break;
-            case 2: //Lightning
-                rsrc = (value.isLocked()) ? "/view/lightninglock.png" : "/view/lightningicn.png";
-                break;
-            case 3: //Release
-                rsrc = (value.isLocked()) ? "/view/releaselock.png" : "/view/releaseicn.png";
-                break;
-            case 4: //Puzzle
-                rsrc = (value.isLocked()) ? "/view/puzzlelock.png" : "/view/puzzleicn.png";
-                break;
-        }
+
+        rsrc = (value.isLocked()) ? "/view/" + value.getType().toLowerCase() +
+                "lock.png" : "/view/" + value.getType().toLowerCase() + "icn.png";
 
         icon = new ImageIcon(getClass().getResource(rsrc));
 
         setIcon(icon);
-        setText(number);
+        setText(description);
         setOpaque(true);
         setBorder(new EmptyBorder(5, 5, 5, 5));
         if (isSelected)
