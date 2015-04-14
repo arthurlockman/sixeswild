572d05d1498ee390ea324f8761b0ea65f511a22b
Arthur Lockman <hello@rthr.me>
2015-04-13 23:13:15 -0400

    Added custom cell renderer for level list.

---
+++ src/main/java/view/LevelCellRenderer.java
@@ -0,0 +1,48 @@
+package view;
+
+import model.Level;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LevelCellRenderer extends JLabel implements ListCellRenderer<Level>
+{
+    @Override
+    public Component getListCellRendererComponent(JList<? extends Level> list, Level value, int index, boolean isSelected, boolean cellHasFocus)
+    {
+        String number = "Level " + value.getNumber();
+        ImageIcon icon;
+        String rsrc = "";
+        switch (value.getType())
+        {
+            case 1: //Elimination
+                rsrc = (value.isLocked()) ? "/view/eliminationlock.png" : "/view/eliminationicn.png";
+                break;
+            case 2: //Lightning
+                rsrc = (value.isLocked()) ? "/view/lightninglock.png" : "/view/lightningicn.png";
+                break;
+            case 3: //Release
+                rsrc = (value.isLocked()) ? "/view/releaselock.png" : "/view/releaseicn.png";
+                break;
+            case 4: //Puzzle
+                rsrc = (value.isLocked()) ? "/view/puzzlelock.png" : "/view/puzzleicn.png";
+                break;
+        }
+
+        icon = new ImageIcon(getClass().getResource(rsrc));
+
+        setIcon(icon);
+        setText(number);
+
+        setOpaque(true);
+        if (isSelected)
+        {
+            setBackground(new Color(86, 95, 182));
+            setForeground(list.getSelectionForeground());
+        } else {
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
+        }
+        return this;
+    }
+}
