c3fd5adb81d7f258f88ce372e6416246df0a11fc
Yiğit Uyan <uyan.yigit@gmail.com>
2015-04-18 17:22:43 -0400

    Created first example controller
    Let it be known that our first controller class is the
    SetSpawnFrequencyController

---
+++ src/main/java/controllers/SetSpawnFrequencyController.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import view.PlayerApplication;
+
+public class SetSpawnFrequencyController
+{
+    PlayerApplication app;
+
+    public SetSpawnFrequencyController(PlayerApplication app)
+    {
+        this.app = app;
+    }
+
+    public void doAction()
+    {
+
+    }
+}
