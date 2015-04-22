b092bb308d32fa8ab8025673c83d3a935a835366
Arthur Lockman <hello@rthr.me>
2015-04-22 17:21:34 -0400

    Added redo test case.

--- src/test/java/controllers/builder/TestUndoRedoController.java
+++ src/test/java/controllers/builder/TestUndoRedoController.java
@@ -34,7 +34,7 @@ public class TestUndoRedoController extends GUITestCase
         }
     }
 
-    public void testUndoMoveController()
+    public void testUndoRedoMoveController()
     {
         window.comboBox("levelTypeSelector").selectItem("Release");
 
@@ -71,5 +71,20 @@ public class TestUndoRedoController extends GUITestCase
         assertEquals(" ", window.label("square0numberlabel").text());
         assertEquals(" ", window.label("square0multiplierlabel").text());
         assertEquals(new Color(200, 200, 200), window.panel("square0").component().getBackground());
+
+        //Redo tiles
+        window.button("redoButton").click();
+        assertEquals("6", window.label("square0numberlabel").text());
+        assertEquals(" ", window.label("square0multiplierlabel").text());
+
+        window.button("redoButton").click();
+        assertEquals("Bckt", window.label("square1numberlabel").text());
+        assertEquals(" ", window.label("square1multiplierlabel").text());
+        assertEquals(new Color(227, 242, 209), window.panel("square1").component().getBackground());
+
+        window.button("redoButton").click();
+        assertEquals(" ", window.label("square80numberlabel").text());
+        assertEquals(" ", window.label("square80multiplierlabel").text());
+        assertEquals(new Color(187, 242, 226), window.panel("square80").component().getBackground());
     }
 }
