329d065445ceb237acf98a14909642a43922b2a0
Yiğit Uyan <uyan.yigit@gmail.com>
2015-04-26 14:50:36 -0400

    Basic BadgeController Work
    Implemented a way to attach the BadgeController and fire doAction method
    in it.

--- src/main/java/controllers/player/CompleteMoveController.java
+++ src/main/java/controllers/player/CompleteMoveController.java
@@ -6,13 +6,20 @@ public class CompleteMoveController
 {
     PlayerApplication app;
 
+    EarnBadgeController badgeController;
+
     public CompleteMoveController(PlayerApplication app)
     {
         this.app = app;
+        badgeController = app.getBadgeController();
     }
 
     public void doAction()
     {
 
+
+        // This is how we fire the doAction method in BadgeController,
+        // If we figure out how to generate Actions, we can also do it that way...
+        badgeController.doAction();
     }
 }
