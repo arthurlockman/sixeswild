c2d2633b5617f7937930f5485eb0a8fc4d214cb8
Arthur Lockman <hello@rthr.me>
2015-04-27 17:52:03 -0400

    Fixed square factory and added square factory tests.

--- src/main/java/model/SquareFactory.java
+++ src/main/java/model/SquareFactory.java
@@ -28,48 +28,19 @@ public class SquareFactory {
 
     protected Board creator;
 
-    /** SquareFactory Constructor with floats */
-    public SquareFactory(float f1, float f2, float f3, float f4, float f5, float f6, float fx1, float fx2, float fx3, Board creator){
-        this.freq1 = (int)(f1 * 100.0);
-        this.freq2 = (int)(f2 * 100.0);
-        this.freq3 = (int)(f3 * 100.0);
-        this.freq4 = (int)(f4 * 100.0);
-        this.freq5 = (int)(f5 * 100.0);
-        this.freq6 = (int)(f6 * 100.0);
-        this.freqx1 = (int)(fx1 * 100.0);
-        this.freqx2 = (int)(fx2 * 100.0);
-        this.freqx3 = (int)(fx3 * 100.0);
-        this.lim1 = (int) (f1*100.0);
-        this.lim2 = (int) (f2*100.0) + this.lim1;
-        this.lim3 = (int) (f3*100.0) + this.lim2;
-        this.lim4 = (int) (f4*100.0) + this.lim3;
-        this.lim5 = (int) (f5*100.0) + this.lim4;
-        this.limx1 = (int) (fx1*100.0);
-        this.limx2 = (int) (fx2*100.0) + this.limx1;
-        this.creator = creator;
-    }
+    protected double totalWeightTiles;
+    protected double totalWeightMult;
+    protected int tiles[] = {1, 2, 3, 4, 5, 6};
+    protected int multipliers[] = {1, 2, 3};
+    protected double[] tileWeights, multWeights;
 
     /** SquareFactory Constructor with integers */
     public SquareFactory(int f1, int f2, int f3, int f4, int f5, int f6, int fx1, int fx2, int fx3, Board creator)
     {
-        this.freq1 = f1;
-        this.freq2 = f2;
-        this.freq3 = f3;
-        this.freq4 = f4;
-        this.freq5 = f5;
-        this.freq6 = f6;
-        this.freqx1 = fx1;
-        this.freqx2 = fx2;
-        this.freqx3 = fx3;
-        this.lim1 = f1;
-        this.lim2 = f2 + this.lim1;
-        this.lim3 = f3 + this.lim2;
-        this.lim4 = f4 + this.lim3;
-        this.lim5 = f5 + this.lim4;
-        limx1 = fx1;
-        lim2 = fx2 + this.limx1;
+        this(f1, f2, f3, f4, f5, f6, fx1, fx2, fx3);
         this.creator = creator;
     }
+
     /** SquareFactory Constructor with integers and no creator */
     public SquareFactory(int f1, int f2, int f3, int f4, int f5, int f6, int fx1, int fx2, int fx3)
     {
@@ -90,6 +61,27 @@ public class SquareFactory {
         limx1 = fx1;
         lim2 = fx2 + this.limx1;
         this.creator = null;
+
+        tileWeights = new double[6];
+        multWeights = new double[3];
+        totalWeightTiles = (double)freq1 / 100.0d;
+        tileWeights[0] = (double)freq1 / 100.0d;
+        totalWeightTiles += (double)freq2 / 100.0d;
+        tileWeights[1] = (double)freq2 / 100.0d;
+        totalWeightTiles += (double)freq3 / 100.0d;
+        tileWeights[2] = (double)freq3 / 100.0d;
+        totalWeightTiles += (double)freq4 / 100.0d;
+        tileWeights[3] = (double)freq4 / 100.0d;
+        totalWeightTiles += (double)freq5 / 100.0d;
+        tileWeights[4] = (double)freq5 / 100.0d;
+        totalWeightTiles += (double)freq6 / 100.0d;
+        tileWeights[5] = (double)freq6 / 100.0d;
+        totalWeightMult = (double)freqx1 / 100.0d;
+        multWeights[0] = (double)freqx1 / 100.0d;
+        totalWeightMult += (double)freqx2 / 100.0d;
+        multWeights[1] = (double)freqx2 / 100.0d;
+        totalWeightMult += (double)freqx3 / 100.0d;
+        multWeights[2] = (double)freqx3 / 100.0d;
     }
 
     /**
@@ -152,40 +144,41 @@ public class SquareFactory {
 
     }
 
-    public Tile genTile(){
-        int tileRoll = (int) (Math.random() * 100) + 1;
-        int multRoll = (int) (Math.random() * 100) + 1;
-
-        int tileVal = 0;
-        int tileMult = 0;
-
-        if(tileRoll < lim1){
-            tileVal = 1;
-        } else if(tileRoll < lim2) {
-            tileVal = 2;
-        } else if(tileRoll < lim3) {
-            tileVal = 3;
-        } else if(tileRoll < lim4) {
-            tileVal = 4;
-        } else if(tileRoll < lim5) {
-            tileVal = 5;
-        } else {
-            tileVal = 6;
-        }
-
-        if(tileVal < 6) {
-            if (multRoll < limx1) {
-                tileMult = 1;
-            } else if (multRoll < limx2) {
-                tileMult = 2;
-            } else {
-                tileMult = 3;
+    /**
+     * Generate a tile according to the weighting policies defined
+     * when the factory is constructed.
+     * @return The newly created tile.
+     */
+    public Tile genTile()
+    {
+        int tile = 0;
+        int mult = 1;
+        int randomIndex = -1;
+        double random = Math.random() * totalWeightTiles;
+        for (int i = 0; i < tileWeights.length; i++)
+        {
+            random -= tileWeights[i];
+            if (random <= 0.0d)
+            {
+                tile = tiles[i];
+                break;
             }
-        } else {
-            tileMult = 1;
         }
-
-        return new Tile(tileVal, tileMult);
+        if (tile != 6)
+        {
+            randomIndex = -1;
+            random = Math.random() * totalWeightMult;
+            for (int i = 0; i < multWeights.length; i++)
+            {
+                random -= multWeights[i];
+                if (random <= 0.0d)
+                {
+                    mult = i + 1;
+                    break;
+                }
+            }
+        }
+        return new Tile(tile, mult);
     }
 
     public int getFreq1()
---
+++ src/test/java/model/TestSquareFactory.java
@@ -0,0 +1,85 @@
+package model;
+
+import junit.framework.TestCase;
+
+/**
+ * Test the frequencies of the square factory's tile generation.
+ *
+ * @author arthurlockman
+ */
+public class TestSquareFactory extends TestCase
+{
+    /**
+     * Generate 1000 tiles and check to make sure that they
+     * are distributed correctly.
+     */
+    public void testSquareFactoryPercentages()
+    {
+        SquareFactory sf = new SquareFactory(16, 16, 16, 16, 16, 20, 33, 33, 34);
+        int count1 = 0, count2 = 0, count3 = 0, count4 = 0, count5 = 0,
+                count6 = 0, countx1 = 0, countx2 = 0, countx3 = 0;
+        int countMult = 0;
+        for (int i = 0; i < 1000; i++)
+        {
+            Tile t = sf.genTile();
+            switch (t.getValue())
+            {
+                case 1:
+                    count1++;
+                    break;
+                case 2:
+                    count2++;
+                    break;
+                case 3:
+                    count3++;
+                    break;
+                case 4:
+                    count4++;
+                    break;
+                case 5:
+                    count5++;
+                    break;
+                case 6:
+                    count6++;
+                    break;
+            }
+            if (t.getValue() != 6)
+            {
+                countMult++;
+                switch (t.getMultiplier())
+                {
+                    case 1:
+                        countx1++;
+                        break;
+                    case 2:
+                        countx2++;
+                        break;
+                    case 3:
+                        countx3++;
+                        break;
+                }
+            }
+        }
+        assertTrue(this.inRange(((double)count1/1000.0), 0.16, 0.1));
+        assertTrue(this.inRange(((double)count2/1000.0), 0.16, 0.1));
+        assertTrue(this.inRange(((double)count3/1000.0), 0.16, 0.1));
+        assertTrue(this.inRange(((double)count4/1000.0), 0.16, 0.1));
+        assertTrue(this.inRange(((double)count5/1000.0), 0.16, 0.1));
+        assertTrue(this.inRange(((double)count6/1000.0), 0.2, 0.1));
+        assertTrue(this.inRange(((double)countx1/(double)countMult), 0.33, 0.1));
+        assertTrue(this.inRange(((double)countx2/(double)countMult), 0.33, 0.1));
+        assertTrue(this.inRange(((double)countx3/(double)countMult), 0.34, 0.1));
+    }
+
+    /**
+     * Determine if a float is within a certain range of another float.
+     * @param number The input number.
+     * @param target The target number.
+     * @param range The range of acceptable values around the target.
+     * @return
+     */
+    public boolean inRange(double number, double target, double range)
+    {
+        return (number <= (target + range) || number >= (target - range));
+    }
+}
