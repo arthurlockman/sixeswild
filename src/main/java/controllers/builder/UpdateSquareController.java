620e100e8da6d6216525fc44e6056a594dd99362
Arthur Lockman <hello@rthr.me>
2015-04-29 11:07:08 -0400

    Fixed builder undo functionality.

--- src/main/java/controllers/builder/UpdateSquareController.java
+++ src/main/java/controllers/builder/UpdateSquareController.java
@@ -40,48 +40,6 @@ public class UpdateSquareController extends MouseAdapter
 
     /**
      *  Updates the Square type as specified by the user's selection.
-     *  @param e:  the mouse event that initiated this move
-     */
-    @Override
-    public void mouseReleased(MouseEvent e)
-    {
-        super.mouseReleased(e);
-        String tileType = "";
-        for (Enumeration<AbstractButton> buttons = tileSelect.getElements(); buttons.hasMoreElements(); )
-        {
-            AbstractButton button = buttons.nextElement();
-
-            if (button.isSelected())
-            {
-                tileType = button.getText();
-            }
-        }
-
-        //Place tile based on selected type.
-        UpdateSquareMove move;
-        if (tileType.equals("Active"))
-        {
-            move = new UpdateSquareMove(new Tile(0, 1), square.getSquare(), true, false);
-        } else if (tileType.equals("Inactive"))
-        {
-            move = new UpdateSquareMove(null, square.getSquare(), false, false);
-        } else if (tileType.equals("Bucket"))
-        {
-            move = new UpdateSquareMove(null, square.getSquare(), true, true);
-        } else if (tileType.equals("Six"))
-        {
-            move = new UpdateSquareMove(new Tile(6, 1), square.getSquare(), true, false);
-        } else {
-            move = new UpdateSquareMove(null, square.getSquare(),
-                    square.getSquare().isActive(), square.getSquare().isBucket());
-        }
-
-        boardView.getBoard().makeMove(move);
-        boardView.refresh();
-    }
-
-    /**
-     *  Updates the Square type as specified by the user's selection.
      *  @param e the mouse event that initiated this move.
      */
     @Override
