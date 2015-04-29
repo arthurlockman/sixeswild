8c38e2be0e2c91b93c87ca5caabd80267b94ece6
Arthur Lockman <hello@rthr.me>
2015-04-29 10:46:19 -0400

    Made level builder less annoying.

--- src/main/java/controllers/builder/UpdateSquareController.java
+++ src/main/java/controllers/builder/UpdateSquareController.java
@@ -38,14 +38,94 @@ public class UpdateSquareController extends MouseAdapter
         this.boardView = app.getBuilderEditorPanel().getBoardViewPanel();
     }
 
-    @Override
     /**
      *  Updates the Square type as specified by the user's selection.
      *  @param e:  the mouse event that initiated this move
      */
-    public void mouseClicked(MouseEvent e)
+    @Override
+    public void mouseReleased(MouseEvent e)
     {
-        super.mouseClicked(e);
+        super.mouseReleased(e);
+        String tileType = "";
+        for (Enumeration<AbstractButton> buttons = tileSelect.getElements(); buttons.hasMoreElements(); )
+        {
+            AbstractButton button = buttons.nextElement();
+
+            if (button.isSelected())
+            {
+                tileType = button.getText();
+            }
+        }
+
+        //Place tile based on selected type.
+        UpdateSquareMove move;
+        if (tileType.equals("Active"))
+        {
+            move = new UpdateSquareMove(new Tile(0, 1), square.getSquare(), true, false);
+        } else if (tileType.equals("Inactive"))
+        {
+            move = new UpdateSquareMove(null, square.getSquare(), false, false);
+        } else if (tileType.equals("Bucket"))
+        {
+            move = new UpdateSquareMove(null, square.getSquare(), true, true);
+        } else if (tileType.equals("Six"))
+        {
+            move = new UpdateSquareMove(new Tile(6, 1), square.getSquare(), true, false);
+        } else {
+            move = new UpdateSquareMove(null, square.getSquare(),
+                    square.getSquare().isActive(), square.getSquare().isBucket());
+        }
+
+        boardView.getBoard().makeMove(move);
+        boardView.refresh();
+    }
+
+    /**
+     *  Updates the Square type as specified by the user's selection.
+     *  @param e the mouse event that initiated this move.
+     */
+    @Override
+    public void mouseEntered(MouseEvent e)
+    {
+        super.mouseEntered(e);
+        if (SwingUtilities.isLeftMouseButton(e)) {
+            String tileType = "";
+            for (Enumeration<AbstractButton> buttons = tileSelect.getElements(); buttons.hasMoreElements(); ) {
+                AbstractButton button = buttons.nextElement();
+
+                if (button.isSelected()) {
+                    tileType = button.getText();
+                }
+            }
+
+            //Place tile based on selected type.
+            UpdateSquareMove move;
+            if (tileType.equals("Active")) {
+                move = new UpdateSquareMove(new Tile(0, 1), square.getSquare(), true, false);
+            } else if (tileType.equals("Inactive")) {
+                move = new UpdateSquareMove(null, square.getSquare(), false, false);
+            } else if (tileType.equals("Bucket")) {
+                move = new UpdateSquareMove(null, square.getSquare(), true, true);
+            } else if (tileType.equals("Six")) {
+                move = new UpdateSquareMove(new Tile(6, 1), square.getSquare(), true, false);
+            } else {
+                move = new UpdateSquareMove(null, square.getSquare(),
+                        square.getSquare().isActive(), square.getSquare().isBucket());
+            }
+
+            boardView.getBoard().makeMove(move);
+            boardView.refresh();
+        }
+    }
+
+    /**
+     *  Updates the Square type as specified by the user's selection.
+     *  @param e:  the mouse event that initiated this move
+     */
+    @Override
+    public void mousePressed(MouseEvent e)
+    {
+        super.mousePressed(e);
         String tileType = "";
         for (Enumeration<AbstractButton> buttons = tileSelect.getElements(); buttons.hasMoreElements(); )
         {
